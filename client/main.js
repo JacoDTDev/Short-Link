b69ee9179423d4d3f7681284d0e0435877c1e3b1
JacoDTDev <jacodtdev@gmail.com>
2019-08-28 10:15:16 +0200

    53. Setting up React-Router

--- client/main.js
+++ client/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Signup from "../imports/ui/Signup";
+import Link from "../imports/ui/Link";
+import Login from "../imports/ui/Login";
+import NotFound from "../imports/ui/NotFound";
 
 Meteor.startup(() => {
   // code to run on client at startup
+    ReactDOM.render(<Signup/>,document.getElementById('app'));
 });
