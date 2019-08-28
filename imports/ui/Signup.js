ce8c832f5193418c0f68b9949495cb9c62581d7d
JacoDTDev <jacodtdev@gmail.com>
2019-08-28 15:55:39 +0200

    57. Creating User Accounts

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from "react-router";
+import {Accounts} from 'meteor/accounts-base';
 
 export default class Signup extends React.Component{
     constructor(props){
@@ -10,18 +11,22 @@ export default class Signup extends React.Component{
     }
     onSubmit(e){
         e.preventDefault();
-
-        this.setState({
-            error: 'Something went wrong.'
-        })
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+        Accounts.createUser({email,password},()=>{
+            console.log('Signup callback', err);
+        });
+        // this.setState({
+        //     error: 'Something went wrong.'
+        // })
     }
     render() {
         return <div>
         <h1>Signup Page</h1>
             {this.state.error? <p>{this.state.error}</p>:undefined}
             <form onSubmit={this.onSubmit.bind(this)}>
-                <input type="text" name="email" placeholder="Email here"/>
-                <input type="password" name="password" placeholder="Password here"/>
+                <input type="text" ref='email' name="email" placeholder="Email here"/>
+                <input type="password" ref='password' name="password" placeholder="Password here"/>
                 <button>Create Account</button>
             </form>
             <p>
