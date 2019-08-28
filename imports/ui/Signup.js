2a9d4798dd15f2d29272af7de41bc37d1ced6d74
JacoDTDev <jacodtdev@gmail.com>
2019-08-28 13:08:05 +0200

    56.1 Component State

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,9 +2,22 @@ import React from 'react';
 import {Link} from "react-router";
 
 export default class Signup extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+          count:0
+        };
+    }
+    increment(){
+        this.setState({
+            count: this.state.count+1
+        });
+    }
     render() {
         return <div>
         <h1>Signup Page</h1>
+            {this.state.count}
+            <p><button onClick={this.increment.bind(this)}>+1</button><button onClick={()=>this.setState({count: this.state.count-1}).bind(this)}>-1</button></p>
             <p>
                 <Link to="/">Already have an account.</Link>
             </p>
