25f41686efcbf86db15aee6962889a3aae8e6d93
JacoDTDev <jacodtdev@gmail.com>
2019-08-28 16:38:48 +0200

    58. Loggin in

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import {Link} from "react-router";
+import {Meteor} from 'meteor/meteor';
 
 export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            error:''
+        };
+    }
+    onSubmit(e){
+        e.preventDefault();
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+
+        Meteor.loginWithPassword({email},password,(err)=>{
+            console.log('Login callback',err)
+        });
+    }
     render() {
         return (<div>
-            <h1>Login to Short lnk</h1>
-            <p>
+                <h1>Short Link</h1>
+
+                {this.state.error? <p>{this.state.error}</p>:undefined}
+
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="text" ref='email' name="email" placeholder="Email here"/>
+                    <input type="password" ref='password' name="password" placeholder="Password here"/>
+                    <button>Login</button>
+                </form>
                 <Link to="/signup">Have an account?</Link>
-            </p>
 
         </div>);
     }
