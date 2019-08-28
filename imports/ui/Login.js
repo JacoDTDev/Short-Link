68e97688da9c147af5907260a2ba8f522db3c638
JacoDTDev <jacodtdev@gmail.com>
2019-08-28 11:51:21 +0200

    55. Navigate Between Routes

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from "react-router";
 
 export default class Login extends React.Component{
     render() {
-        return <div>
-            <h1>Login Page</h1>
-        </div>
+        return (<div>
+            <h1>Login to Short lnk</h1>
+            <p>
+                <Link to="/signup">Have an account?</Link>
+            </p>
+
+        </div>);
     }
 }
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from "react-router";
 
 export default class Signup extends React.Component{
     render() {
         return <div>
         <h1>Signup Page</h1>
+            <p>
+                <Link to="/">Already have an account.</Link>
+            </p>
         </div>
     }
 }
