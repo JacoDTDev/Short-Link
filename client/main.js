2c5b2e91d6e438b1afdc03d176fb9f55e2ead807
JacoDTDev <jacodtdev@gmail.com>
2019-09-03 12:42:34 +0200

    64. Refactoring Router Code

--- client/main.js
+++ client/main.js
@@ -1,49 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
 import ReactDOM from 'react-dom'
-import {Router,Route,browserHistory} from "react-router";
 import{Tracker} from "meteor/tracker";
 
-import Signup from "../imports/ui/Signup";
-import Link from "../imports/ui/Link";
-import Login from "../imports/ui/Login";
-import NotFound from "../imports/ui/NotFound";
+import{routes,onAuthChange} from "../imports/routes/routes";
 
-//pages we access if not authenticated
-const unauthenticatedPages =['/','/signup'];
-//pages we want to access if authenticated
-const authenticatedPages=['/links'];
-const onEnterPublicPage = ()=>{
-    //check if user is already logged in
-    if(Meteor.userId()){
-        browserHistory.replace('/links');
-    }
-};
-const onEnterPrivatePage=()=>{
-    if(!Meteor.userId()){
-        browserHistory.replace('/');
-    }
-};
-const routes=(
-    <Router history={browserHistory}>
-        <Route path='/' component={Login} onEnter={onEnterPublicPage}/>
-        <Route path='/signup' component={Signup} onEnter={onEnterPublicPage}/>
-        <Route path='/links' component={Link} onEnter={onEnterPrivatePage} />
-        <Route path='*' component={NotFound}/>
-    </Router>
-);
-//window.browserHistory = browserHistory;
 Tracker.autorun(()=>{
    const isAuthenticated = !!Meteor.userId();
-   const pathname =browserHistory.getCurrentLocation().pathname;
-   //so here we check if the current page is in the array for UnauthenticatedPages
-   const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
-   const isAuthenticatedPage = authenticatedPages.includes(pathname);
-   if(isUnauthenticatedPage && isAuthenticated){
-       browserHistory.replace('/links');
-   }else if(isAuthenticatedPage && !isAuthenticated){
-       browserHistory.replace('/');
-   }
+   onAuthChange(isAuthenticated);
 });
 
 Meteor.startup(() => {
