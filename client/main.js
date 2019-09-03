45da5dcedd002df0b12a27b4399f0b59633e4560
JacoDTDev <jacodtdev@gmail.com>
2019-09-03 09:00:28 +0200

    59. Logging Out and Tracking Auth Status

--- client/main.js
+++ client/main.js
@@ -2,11 +2,17 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom'
 import {Router,Route,browserHistory} from "react-router";
+import{Tracker} from "meteor/tracker";
 
 import Signup from "../imports/ui/Signup";
 import Link from "../imports/ui/Link";
 import Login from "../imports/ui/Login";
 import NotFound from "../imports/ui/NotFound";
+
+//pages we access if not authenticated
+const unauthenticatedPages =['/','/signup'];
+//pages we want to access if authenticated
+const authenticatedPages=['/links'];
 
 const routes=(
     <Router history={browserHistory}>
@@ -16,6 +22,19 @@ const routes=(
         <Route path='*' component={NotFound}/>
     </Router>
 );
+//window.browserHistory = browserHistory;
+Tracker.autorun(()=>{
+   const isAuthenticated = !!Meteor.userId();
+   const pathname =browserHistory.getCurrentLocation().pathname;
+   //so here we check if the current page is in the array for UnauthenticatedPages
+   const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+   const isAuthenticatedPage = authenticatedPages.includes(pathname);
+   if(isUnauthenticatedPage && isAuthenticated){
+       browserHistory.push('/links');
+   }else if(isAuthenticatedPage && !isAuthenticated){
+       browserHistory.push('/');
+   }
+});
 
 Meteor.startup(() => {
   // code to run on client at startup
