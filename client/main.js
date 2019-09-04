e1bd06a5acdd9407e30435000cb28228e3cc2e0e
JacoDTDev <jacodtdev@gmail.com>
2019-09-04 13:59:13 +0200

    69. part 2

--- client/main.js
+++ client/main.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom'
 import{Tracker} from "meteor/tracker";
 
 import{routes,onAuthChange} from "../imports/routes/routes";
-import{Links} from "../imports/api/links";
+
 
 Tracker.autorun(()=>{
    const isAuthenticated = !!Meteor.userId();
    onAuthChange(isAuthenticated);
-});
-
-Tracker.autorun(()=>{
-    const links = Links.find().fetch();
-    console.log('new links',Links);
 });
 
 Meteor.startup(() => {
