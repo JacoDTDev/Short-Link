217875414bd01fd60628241c815f5a5bfdb4df49
JacoDTDev <jacodtdev@gmail.com>
2019-09-04 14:52:36 +0200

    70. Creating Publications and Subscriptions

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,3 +1,9 @@
+import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
 
 export const Links = new Mongo.Collection('links');
+if(Meteor.isServer){
+    Meteor.publish('links',()=>{
+        return Links.find();
+    });
+}
