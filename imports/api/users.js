1202066cef98838616ee4de3d028f5db3265a816
JacoDTDev <jacodtdev@gmail.com>
2019-09-05 12:58:55 +0200

    74. Method Validation

--- imports/api/users.js
+++ imports/api/users.js
@@ -5,15 +5,12 @@ import {Meteor} from "meteor/meteor";
 Accounts.validateNewUser((user)=>{
     const email = user.emails[0].address;
 
-    try{
-        new SimpleSchema({
+    new SimpleSchema({
             email:{
                 type: String,
                 regEx: SimpleSchema.RegEx.Email
             }
-        }).validate({email});
-    }catch (e) {
-        throw new Meteor.Error(400, e.message);
-    }
+    }).validate({email});
+
     return true;
 });
