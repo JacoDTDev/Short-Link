ecea750d43f2bdbf348d192cf29980676846fffe
JacoDTDev <jacodtdev@gmail.com>
2019-09-05 14:39:06 +0200

    75. Node HTTP and Connect

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import {WebApp} from 'meteor/webapp';
 
 import'../imports/api/users';
 import '../imports/api/links';
@@ -6,5 +7,17 @@ import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-    Meteor.call('greetUser',(err,res)=>{console.log('Greet user Arument',err,res)});
+  WebApp.connectHandlers.use((req,res, next)=>{
+      console.log('This is from my custom middleware');
+      console.log(req.url, req.method, req.headers, req.query);
+      //set HTTP status code
+      res.statuseCode =404;
+      //Set HTTP headers
+      res.setHeader('my-custom-header','Andrew was here!');
+      //Set HTTP body
+      res.write('<h1>This is my middleware at work!</h1>');
+      //End HTTP request
+      res.end();
+      next();
+  });
 });
