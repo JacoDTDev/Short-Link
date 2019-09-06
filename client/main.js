632d1a764c46876049b3946eb283bfee10993e9c
JacoDTDev <jacodtdev@gmail.com>
2019-09-06 12:08:27 +0200

    80.1 Stateless Functional Components

--- client/main.js
+++ client/main.js
@@ -1,16 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom'
 import{Tracker} from "meteor/tracker";
+import React from "react";
 
 import{routes,onAuthChange} from "../imports/routes/routes";
 import '../imports/startup/simple-schema-configuration.js';
+
 
 Tracker.autorun(()=>{
    const isAuthenticated = !!Meteor.userId();
    onAuthChange(isAuthenticated);
 });
 
+//Stateless functional components
+//dont have states but do have props
+const MyComponent =(props) =>{
+    return (
+        <div>
+            <h1>MyComponent is here!{props.name}</h1>
+        </div>
+    );
+};
+
 Meteor.startup(() => {
   // code to run on client at startup
-    ReactDOM.render(routes, document.getElementById('app'));
+    ReactDOM.render(<MyComponent name='Tom'/>, document.getElementById('app'));
 });
