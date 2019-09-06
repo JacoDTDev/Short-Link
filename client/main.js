e004e6f1e3a941bc6236c16474e3919fc9ac757b
JacoDTDev <jacodtdev@gmail.com>
2019-09-06 13:15:53 +0200

    80.2 Stateless Functional Components

--- client/main.js
+++ client/main.js
@@ -6,23 +6,12 @@ import React from "react";
 import{routes,onAuthChange} from "../imports/routes/routes";
 import '../imports/startup/simple-schema-configuration.js';
 
-
 Tracker.autorun(()=>{
    const isAuthenticated = !!Meteor.userId();
    onAuthChange(isAuthenticated);
 });
 
-//Stateless functional components
-//dont have states but do have props
-const MyComponent =(props) =>{
-    return (
-        <div>
-            <h1>MyComponent is here!{props.name}</h1>
-        </div>
-    );
-};
-
 Meteor.startup(() => {
   // code to run on client at startup
-    ReactDOM.render(<MyComponent name='Tom'/>, document.getElementById('app'));
+    ReactDOM.render(routes, document.getElementById('app'));
 });
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -3,13 +3,20 @@ import React from 'react';
 import LinksList from "./LinksList";
 import PrivateHeader from "./PrivateHeader";
 import AddLink from "./AddLink";
-
-export default class Link extends React.Component{
-    render() {
-        return <div>
-            <PrivateHeader title='Your Links'/>
-            <LinksList/>
-            <AddLink/>
-        </div>
-    }
+// changed to a stateless functional component
+export default ()=>{
+    return <div>
+        <PrivateHeader title='Your Links'/>
+        <LinksList/>
+        <AddLink/>
+    </div>
 }
+// export default class Link extends React.Component{
+//     render() {
+//         return <div>
+//             <PrivateHeader title='Your Links'/>
+//             <LinksList/>
+//             <AddLink/>
+//         </div>
+//     }
+// }
