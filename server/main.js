027e52550c7a74bcdcbb2094784f817f20141552
JacoDTDev <jacodtdev@gmail.com>
2019-09-06 10:05:46 +0200

    76. Redirecting Shortened Links

--- server/main.js
+++ server/main.js
@@ -2,22 +2,26 @@ import { Meteor } from 'meteor/meteor';
 import {WebApp} from 'meteor/webapp';
 
 import'../imports/api/users';
-import '../imports/api/links';
+import {Links} from '../imports/api/links';
 import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  WebApp.connectHandlers.use((req,res, next)=>{
-      console.log('This is from my custom middleware');
-      console.log(req.url, req.method, req.headers, req.query);
-      //set HTTP status code
-      res.statuseCode =404;
-      //Set HTTP headers
-      res.setHeader('my-custom-header','Andrew was here!');
-      //Set HTTP body
-      res.write('<h1>This is my middleware at work!</h1>');
-      //End HTTP request
-      res.end();
-      next();
-  });
+    //middle ware to take us to google.com
+    //set HTTP status code to A 302
+    //sET LOCATION HEADER TO HTTP:
+    WebApp.connectHandlers.use((req, res, next)=>{
+        const _id = req.url.slice(1);
+        const link = Links.findOne({_id});
+
+        if(link){
+            //if link is found
+             res.statusCode= 302;
+             res.setHeader('location',link.url);
+             res.end();
+
+        }else{
+            next()
+        }
+    });
 });
