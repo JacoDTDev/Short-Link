bb273f0ab83d43b30f78171abeba530a2e073f95
JacoDTDev <jacodtdev@gmail.com>
2019-09-09 16:03:39 +0200

    87. Controlled and Uncontrolled input

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,24 +2,45 @@ import React from "react";
 import {Meteor} from "meteor/meteor";
 
 export default class AddLink extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            url:''
+        };
+    }
     onFormSubmit(e){
-        const url = this.refs.url.value.trim();
-
+        //const url = this.state.url; //same as bottom statement
+        const {url} =this.state;
         e.preventDefault();
 
         if(url){
             // not used anymore, use method instead   Links.insert({url, userId: Meteor.userId()});
-            Meteor.call('links.insert',url); //we dont always have to add a callback method like shown here
+            Meteor.call('links.insert',url,(err,res)=>{
+                if(!err){
+                    this.setState({url:''});
+                }
+            });
             //to clear
-            this.ref.url.value='';
+
         }
+    }
+
+    onFormChange(e){
+        this.setState({
+            url:e.target.value.trim()
+        });
     }
     render(){
         return(
             <dev>
                 <p>Add Link</p>
                 <form onSubmit={this.onFormSubmit.bind(this)}>
-                    <input type="text" ref="url" placeholder="URL here"/>
+                    <input
+                        type="text"
+                        placeholder="URL"
+                        value={this.state.url}
+                        onChange={this.onFormChange.bind(this)}
+                    />
                     <button>Add Link</button>
                 </form>
             </dev>
--- imports/ui/LinkListFilters.js
+++ imports/ui/LinkListFilters.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import {Session} from 'meteor/session';
+import {Tracker} from 'meteor/tracker';
 import {Meteor} from "meteor/meteor";
-//to create the check box
-export default()=>{
-    return(
-        <div>
-          <lable>
-              <input type='checkbox' onChange={(e)=>{
-                  console.log(e.target.checked);
-                 Session.set('showVisible',!e.target.checked);
-              }}/>
-              show hidden links
-          </lable>
-        </div>
-    );
-};
+
+export default class LinkedListFilers extends React.Component{
+    constructor(pops){
+        super(pops);
+        this.state ={
+            showVisible: true
+        };
+    }
+    componentDidMount() {
+        this.tracker = Tracker.autorun(()=>{
+            const showVisible = Session.get('showVisible');
+            this.setState({
+                showVisible
+            })
+        })
+    }
+    componentWillUnmount() {
+        this.tracker.stop();
+    }
+
+    render() {
+        return(
+            <div>
+                <lable>
+                    <input type='checkbox' checked={!this.state.showVisible} onChange={(e)=>{
+                        console.log(e.target.checked);
+                        Session.set('showVisible',!e.target.checked);
+                    }}/>
+                    show hidden links
+                </lable>
+            </div>
+        );
+    }
+}
