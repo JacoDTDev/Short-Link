947331a17fc4351a432773e528381cfb61067eab
JacoDTDev <jacodtdev@gmail.com>
2019-09-09 09:22:43 +0200

    83: State Changing

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            justCopied:false
+        };
+    }
+
     componentDidMount() {
        this.clipboard = new Clipboard(this.refs.copy);
 
        this.clipboard.on('success',()=>{
-           alert("It worked!")
+          this.setState({justCopied:true}) ;
+
+          setTimeout(()=>this.setState({justCopied:false}),1000);
+          // alert("It worked!")
        }).on('error',()=>{
            alert('Unable to copy.Please manually copy the link')
        })
     }
+
     componentWillUnmount() {
         this.clipboard.destroy();
     }
@@ -20,7 +31,7 @@ export default class LinksListItem extends React.Component{
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
-                <button ref='copy' data-clipboard-text={this.props.shortUrl}>Copy</button>
+                <button ref='copy' data-clipboard-text={this.props.shortUrl}>{this.state.justCopied? 'Copied':'Copy'}</button>
             </div>
         );
     }
