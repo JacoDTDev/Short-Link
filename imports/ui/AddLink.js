34ea72f7dd9eb964946c8b86c2afb83e483296c9
JacoDTDev <jacodtdev@gmail.com>
2019-09-10 11:58:49 +0200

    91. Setting up Modal Errors Part1

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -7,24 +7,22 @@ export default class AddLink extends React.Component{
         super(props);
         this.state = {
             url:'',
-            isOpen: false
+            isOpen: false,
+            error:''
         };
     }
     onFormSubmit(e){
-        //const url = this.state.url; //same as bottom statement
         const {url} =this.state;
+
         e.preventDefault();
 
-        if(url){
-            // not used anymore, use method instead   Links.insert({url, userId: Meteor.userId()});
-            Meteor.call('links.insert',url,(err,res)=>{
-                if(!err){
-                    this.setState({url:'', isOpen: false});
-                }
-            });
-            //to clear
-
-        }
+        Meteor.call('links.insert',url,(err,res)=>{
+            if(!err){
+                this.setState({url:'', isOpen: false, error:''});
+            }else{
+                this.setState({error: err.reason});
+            }
+        });
     }
 
     onFormChange(e){
@@ -36,18 +34,24 @@ export default class AddLink extends React.Component{
         return(
             <dev>
                 <button onClick={()=>{this.setState({isOpen:true})}}>+ Add Link</button>
-                <Modal isOpen={this.state.isOpen} contentLabel="Add Link">
-                    <p>Add Link</p>
+                <Modal
+                    isOpen={this.state.isOpen}
+                    contentLabel="Add Link"
+                    onAfterOpen={()=>{this.refs.url.focus()}}
+                    onRequestClose={()=>this.setState({isOpen:false, url:'', error:''})}>
+                    <h1>Add Link</h1>
+                    {this.state.error ? <p>this.state.error</p>:undefined}
                     <form onSubmit={this.onFormSubmit.bind(this)}>
                         <input
                             type="text"
                             placeholder="URL"
+                            ref="url"
                             value={this.state.url}
                             onChange={this.onFormChange.bind(this)}
                         />
                         <button>Add Link</button>
                     </form>
-                    <button onClick={()=>{this.setState({isOpen:false, url:''})}}>Cancel</button>
+                    <button onClick={()=>{this.setState({isOpen:false, url:'', error:''})}}>Cancel</button>
                 </Modal>
             </dev>
         );
